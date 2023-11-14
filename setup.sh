595fac5ab9b974c9ece26b054cf1a64e351525be
Johannes Skov Frandsen <jsf@greenoak.dk>
2023-11-14 19:35:52 +0000

    tweaks for puml rendering

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
  #!/usr/bin/env bash
 
-docker pull plantuml/plantuml-server:jetty-v1.2023.1
+docker pull plantuml/plantuml-server:jetty-v1.2023.12
 sudo apt-get -y update
 sudo apt-get -y install graphviz
 npm install --location=global npm@latest typescript@latest markdownlint-cli@latest
+wget https://raw.githubusercontent.com/plantuml-stdlib/C4-PlantUML/master/C4_Component.puml
